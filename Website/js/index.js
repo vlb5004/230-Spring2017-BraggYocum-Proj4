ec7c472c402b0ccd614ba3d28dc4c45b6cce6a74
Victoria Bragg <vlb5004@g.rit.edu>
2017-05-14 19:29:30 -0400

    Uploading a lot of changes
    Maddie and I worked on files together

--- Website/js/index.js
+++ Website/js/index.js
@@ -10,12 +10,85 @@ $(document).ready(function() {
 		navigationPosition: 'right',
 		//Scrolling
 		css3: true,
-		scrollingSpeed: 700,
+		scrollingSpeed: 600,
 		autoScrolling: true,
 		fitToSection: true,
 		fitToSectionDelay: 1000,
 		scrollBar: false,
 		easing: 'easeInOutCubic',
 		easingcss3: 'ease',
+        
+        // anchors
+        anchors:['first', 'second', 'third'],
+        
+        afterLoad: function(anchorLink, index){
+            var loadedSection = $(this);
+            
+            //fire animations for first wonder
+            if(anchorLink == 'first'){
+                caseOneAnimations();
+            }
+
+            //fire animations for second wonder
+            if(anchorLink == 'second'){
+                caseTwoAnimations();
+            }
+            
+            //fire animations for third wonder
+            if(anchorLink == 'third'){
+                caseThreeAnimations();
+            }
+        },
+        
 	});
 });
+
+
+// variables for first wonder
+
+var image1 = $('.area .image1');
+
+// setup animations to be fired on first wonder
+var caseOneAnimations = (function() {
+    
+    setTimeout(function() { 
+        image1.addClass('visible'); 
+    }, 150);
+});
+
+
+// variables for second wonder
+
+var image2 = $('.area .image2');
+
+
+// setup animations to be fired on second wonder
+
+var caseTwoAnimations = (function() {
+    
+    setTimeout(function() { 
+        image2.addClass('visible'); 
+    }, 150);
+});
+
+
+
+// variables for third wonder
+
+var image3 = $('.area .image3');
+
+
+// setup animations to be fired on third wonder
+
+var caseThreeAnimations = (function() {
+    
+   setTimeout(function() { 
+        image3.addClass('visible'); 
+    }, 150);
+});
+
+
+
+
+
+
